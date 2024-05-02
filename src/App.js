2742cf86fa830dee6c16a2165d2fad7ed4d56f31
karwanm <karwan.ismo@gmail.com>
2024-05-02 20:41:58 +0100

    Setting up react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Home</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<h1>Home</h1>}/>
+          <Route path="/home" element={<h1>Home</h1>}/>
+          <Route path="/about" element={<h1>About</h1>}/>
+          <Route path="/contact" element={<h1>Contact</h1>}/>
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
