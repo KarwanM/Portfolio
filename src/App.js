647a09b71a35c1dc07c0047c8a8ab7200e324a46
karwanm <karwan.ismo@gmail.com>
2024-05-02 21:27:27 +0100

    Add header links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
-import Projects from "./pages/Projects/Projects";
 import Footer from "./components/Footer/Footer";
 
 const App = () => {
@@ -12,8 +11,6 @@ const App = () => {
         <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
         </Routes>
         <Footer />
       </Router>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <div id="about">
+              About
+            </div>
+          </li>
+          <li>
+            <div id="portfolio">
+              Projects
+            </div>
+          </li>
+          <li>
+            <div id="contact">
+              Contact
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
