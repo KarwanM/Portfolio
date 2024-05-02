d2e8ffe64a2ac08090c4bb8b9ae941fba6fae749
karwanm <karwan.ismo@gmail.com>
2024-05-02 20:49:27 +0100

    Adding header and footer compenents

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
 const App = () => {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
-          <Route exact path="/" element={<h1>Home</h1>}/>
-          <Route path="/home" element={<h1>Home</h1>}/>
-          <Route path="/about" element={<h1>About</h1>}/>
-          <Route path="/contact" element={<h1>Contact</h1>}/>
+          <Route exact path="/" element={<h1>Home</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
