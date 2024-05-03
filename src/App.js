4e33057a9d5c0254cf25b067f002e26c0500e8e2
karwanm <karwan.ismo@gmail.com>
2024-05-03 22:26:30 +0100

    Update colors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import "./app.scss"
+
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
