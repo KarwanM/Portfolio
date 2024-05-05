5eb363b3716b9f4e5256d82b408f90a6dbe09417
karwanm <karwan.ismo@gmail.com>
2024-05-05 23:46:24 +0100

    Add banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./banner.scss"
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <p>
+        Hi There, <span>I'm</span>
+      </p>
+      <h1>Karwan Ismael</h1>
+      <div className="banner-animated-text">
+        <h3>
+          <span>Software Engineer</span>
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Banner from "../../components/Banner/Banner";
 import About from "../../components/About/About";
 import Project from "../../components/Project/Project";
 import Contact from "../../components/Contact/Contact";
@@ -9,6 +10,7 @@ import "./home.scss";
 const Home = () => {
   return (
     <main>
+      <Banner />
       <About />
       <Project />
       <Contact />
