d8d42ee7331a5ae5eb131e1136d169cb8ce881f6
karwanm <karwan.ismo@gmail.com>
2024-05-09 21:50:05 +0100

    Add about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+
+import "./about.scss";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about-section">
+      <h1>Software Engineer</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea autem, ex
+        non veritatis minus eligendi laborum fuga neque totam, sunt sed, ullam
+        culpa sapiente esse molestias rerum amet eius ipsum! Lorem ipsum dolor
+        sit amet, consectetur adipisicing elit. Provident reprehenderit
+        excepturi ullam. Ut voluptate odio fuga, excepturi sapiente assumenda
+        sint asperiores necessitatibus, non dolores, culpa eum autem voluptates
+        officia atque! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Possimus quibusdam dolorem suscipit molestias praesentium, explicabo aut
+        eum debitis enim illo laudantium maiores soluta iure, placeat molestiae
+        nam distinctio esse recusandae.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
