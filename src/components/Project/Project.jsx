c00fd66388354c72406b452d35b65e0a994cc7d2
karwanm <karwan.ismo@gmail.com>
2024-05-10 22:07:17 +0100

    Add projects placeholder

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+const projects = [1, 2, 3, 4, 5, 6];
+
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <div>
+      <h1>Projects</h1>
+      {projects.map((number) => {
+        return <div>{number}</div>;
+      })}
+    </div>
+  );
 };
 
 export default Project;
