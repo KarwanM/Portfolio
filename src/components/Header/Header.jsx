e8b8540b8b00b8b4b48db6e1b7b5220f6b493c2a
karwanm <karwan.ismo@gmail.com>
2024-05-11 22:49:58 +0100

    Make some changes to navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./header.scss";
@@ -6,32 +6,61 @@ import "./header.scss";
 import searchIcon from "../../assets/icons/search.png";
 
 const Header = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleDropdown = () => {
+    setDropdown(!dropdown);
+  };
+
+  const handleClick = () => {
+    setDropdown(!dropdown);
+  };
+
   return (
     <header>
       <nav>
-        <ul>
-          <li>
-            <div>
-              <Link id="home" to="/">
-                KI
-              </Link>
-            </div>
-          </li>
-          <li>
-            <div id="about">About</div>
-          </li>
-          <li>
-            <div id="projects">Projects</div>
-          </li>
-          <li>
-            <div id="skills">Skills</div>
-          </li>
-          <li>
-            <div id="contact">Contact</div>
-          </li>
-        </ul>
-        <div className="search">
-          <img src={searchIcon} alt="Search Icon" />
+        <div onClick={handleDropdown} className="dropdown">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div className="nav-left">
+          <div id="home" onClick={""}>
+            KI Portfolio
+          </div>
+        </div>
+        <div
+          onClick={handleClick}
+          className={
+            dropdown ? "nav-right show-dropdown" : "nav-right hide-dropdown"
+          }
+        >
+          <ul>
+            <li>
+              <div id="home" onClick={""}>
+                Home
+              </div>
+            </li>
+            <li>
+              <div id="about" onClick={""}>
+                About
+              </div>
+            </li>
+            <li>
+              <div id="projects" onClick={""}>
+                Portfolio
+              </div>
+            </li>
+            <li>
+              <div id="contact" onClick={""}>
+                Contact
+              </div>
+            </li>
+          </ul>
+          <div className="search">
+            <img src={searchIcon} alt="Search Icon" />
+          </div>
         </div>
       </nav>
     </header>
